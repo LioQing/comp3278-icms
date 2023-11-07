7fc0ad663d409e4c6c989ab9835e0ccf78bf6a5b
timmychan823 <87014781+timmychan823@users.noreply.github.com>
2023-11-08 01:14:36 +0800

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path("ping", views.PingPongView.as_view(), name="ping"),
-    path("student/", views.StudentView.get, name = "username"),
+    path("students/<str:username>/", views.StudentView.as_view(), name = "students"),
 ]
